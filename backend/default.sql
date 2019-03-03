a975f72e277e68c8901cf07f0bcdb62d124ab2fc
Nathan Corbyn <me@nathancorbyn.com>
2019-03-03 19:17:54 +0000

    Update achievements (#55)

--- backend/default.sql
+++ backend/default.sql
@@ -3,17 +3,26 @@
 DELETE FROM achieved;
 DELETE FROM achievements;
 INSERT INTO achievements (name, description, reward) VALUES 
-    ("Welcome to my mine", "Collect ALL the ore", 11),
-    ("Ocean man", "Catch ALL the fish", 101),
-    ("Mourning wood", "Kill ALL the tree", 131),
-    ("Yeti slayer", "Sneak past the yeti", 151),
-    ("Yak whisperer", "Tip the yak over", 181),
-    ("TNT I'm Zygomite", "Calm down the Zygomite", 191),
-    ("Crab rave", "Tap the crab just right", 313),
-    ("King of the Slimes", "Defeat the king slime", 353),
-    ("Mr. smartypants", "Solve ALL the puzzles", 373);
+    ("Taking inventory...",  "Open the inventory",          1),
+    ("Grumpy",               "More ore!",                   1),
+    ("Bashful",              "More ore!",                   1),
+    ("Dopey",                "More ore!",                   1),
+    ("It's all mine",        "Collect ALL the ore",        11),
+    ("Finding Nome",         "Blub blub blub...",           1),
+    ("Finding Dyro",         "Blub blub blub...",           1),
+    ("Ocean man",            "Catch ALL the fish",        101),
+    ("Run Forest, run!",     "Bark bark bark...",           1),
+    ("Timber!!!",            "Bark bark bark...",           1),
+    ("It's treeson!",        "Bark bark bark...",           1),
+    ("Mourning wood",        "Kill ALL the trees",        131),
+    ("What a steal!",        "Pickpocket the yeti",       151),
+    ("Yak whisperer",        "Tip the yak over",          181),
+    ("TNT I'm Zygomite",     "Calm down the Zygomite",    191),
+    ("Crab rave",            "Tap the crab just right",   313),
+    ("King of the Slimes",   "Defeat the king slime",     353),
+    ("Mr. smartypants",      "Solve ALL the puzzles",     373);
 
 DELETE FROM content;
 INSERT INTO content (name, enabled) VALUES
-    ("resource", 1),
+    ("resource",  1),
     ("challenge", 1);
